2d531ebb71c66d81a5d003d7601fe8a4fd7e8f67
Christopher Lukas kverne <61789566+chriskverne@users.noreply.github.com>
2024-05-05 00:02:32 -0400

    Delete Dentry.h

--- Dentry.h
+++
@@ -1,9 +0,0 @@
-// DEntry Structure
-typedef struct {
-    unsigned long long key_hash; 
-    int type;
-    int klen;
-    int vlen;
-    char key[256];  // Array of bytes to store the key
-    int val;
-} DEntry;
